fa04a3f719e5f6b0ef2745c13228546ba164f841
Eric Haughee <ehaughee@gmail.com>
2013-03-28 21:08:31 +0000

    added socket io, need to fix messages/messagebox

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -33,3 +35,32 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+/**
+ * Socket.IO Code
+ */
+io.sockets.on('connection', function (socket) {
+
+  socket.on('sendcode', function(data) {
+    io.sockets.emit('updatecode', socket.username, data);
+  });
+  
+  socket.on('sendchat', function (data) {
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	socket.on('adduser', function(username){
+		socket.username = username;
+		usernames[username] = username;
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	socket.on('disconnect', function(){
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers', usernames);
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+});
+
