82b25358ba53d13c034f6cd17c8cb4b7ad321806
Eric Haughee <ehaughee@gmail.com>
2013-03-29 14:54:44 +0000

    chat working with many bugs

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -32,13 +30,16 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
 /**
  * Socket.IO Code
  */
+var io = require('socket.io').listen(server);
+
+var usernames = {};
 io.sockets.on('connection', function (socket) {
 
   socket.on('sendcode', function(data) {
