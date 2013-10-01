0aac8b9da6bc2a11c119572b0d44453b1ec05116
Eric Haughee <ehaughee@gmail.com>
2013-09-30 21:09:09 -0700

    another try

--- install.js
+++ install.js
@@ -1,23 +1,27 @@
 var cmd = require("child_process").exec;
 cmd("cd public && bower install", 
   function (error, stdout) {
+    console.log("Bower dependencies");
     if (error !== null) {
       console.log("exec error: " + error);
     }
     else {
       console.log("stdout: " + stdout);
-      cmd("cd bower_components/ace && npm install && node Makefile.dryice.js",
+      cmd("cd public/bower_components/ace && npm install && node Makefile.dryice.js",
         function (error, stdout) {
+          console.log("Build Ace");
           if (error !== null) {
             console.log("exec error: " + error);
           }
           else {
             console.log("stdout: " + stdout);
-            cmd("cd ../socket.io-client && npm install && make", 
+            cmd("cd public/bower_components/socket.io-client && npm install && make", 
               function (error, stdout) {
+                console.log("Build Socket.io-client");
                 if (error !== null) {
                   console.log("exec error: " + error);
                 }
+              }
             );
           }
         }
