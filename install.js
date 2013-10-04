f5fc6059b25b6d4027e6aa464a07ec2671ed59c0
Eric Haughee <ehaughee@gmail.com>
2013-10-03 20:44:30 -0700

    trying to fix the install script again...its like 4 or 5 times now probably more

--- install.js
+++ install.js
@@ -8,7 +8,7 @@ cmd("cd ./public && bower install && ls ./bower_components/",
     else {
       console.log("stdout: " + stdout);
       console.log("stderr: " + stderr);
-      cmd("cd ./public/bower_components/ace && npm install && node Makefile.dryice.js",
+      cmd("cd ./public/bower_components/ace && npm install --production && node Makefile.dryice.js",
         function (error, stdout, stderr) {
           console.log("Build Ace");
           if (error !== null) {
